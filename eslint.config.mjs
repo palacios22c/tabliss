19a195f26a83e6d5555847e286898bc471704b90
BookCatKid <99609593+BookCatKid@users.noreply.github.com>
2026-01-24 16:11:57 -0800

    chore: tell eslint to shut up about require and fix node definitions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import eslintReact from "@eslint-react/eslint-plugin";
 import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config} */
@@ -13,6 +14,17 @@ export default defineConfig(
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/ban-ts-comment": "warn",
+    },
+  },
+  {
+    files: ["webpack.config.js", "jest.config.json", "scripts/**/*.js", "eslint.config.mjs"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
     },
   },
 );
